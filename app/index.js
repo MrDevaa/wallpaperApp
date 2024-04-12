533d429af85341dc2c441a52703006037a83f1aa
Devanandhan Codandabany <d.codandabany@gmail.com>
2024-04-12 18:47:57 +0200

    set configuration added react reanimated plugin and expo vector/icons (npm)

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const welcomeScreen = () => {
+  return (
+    <View>
+      <Text>welcomeScreen</Text>
+    </View>
+  )
+}
+
+export default welcomeScreen
