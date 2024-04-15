59c39f41b9e85c3d05f086c061c660d6b3bbc7f5
Devanandhan Codandabany <d.codandabany@gmail.com>
2024-04-15 16:10:49 +0200

    searchBar / used use state and useRef / searchinputRef

--- app/home/index.js
+++ app/home/index.js
@@ -1,14 +1,18 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Pressable, ScrollView, TextInput  } from 'react-native'
+import React, { useRef, useState } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { FontAwesome6 } from '@expo/vector-icons'
-import { theme } from '../../constants/theme'
-import { hp, wp } from '../../helpers/common'
+import { Feather, FontAwesome6, Ionicons } from '@expo/vector-icons';
+import { theme } from '../../constants/theme';
+import { hp, wp } from '../../helpers/common';
 
 
 const HomeScreen = () => {
+
   const {top} = useSafeAreaInsets();
   const paddingTop = top>0? top+10: 30;
+  const [search, setSearch] = useState('');
+  const searchInputRef = useRef(null);
+
   return (
     <View style={[styles.container, {paddingTop}]}>
       {/*Header */}
@@ -22,6 +26,33 @@ const HomeScreen = () => {
                 <FontAwesome6 name="bars-staggered" size={22} color={theme.colors.neutral(0.7)} />
               </Pressable>
         </View>
+
+    <ScrollView
+      contentContainerStyle={{gap: 15}}
+    
+    >
+      {/*Search Bar */}
+      <View style={styles.searchBar}>
+        <View style={styles.searchIcon}>
+          <Feather name='search' size={24} color={theme.colors.neutral(0.4)} />
+        </View>
+        <TextInput
+            placeholder='Search for wallpapers...'
+            value={search}
+            ref={searchInputRef}
+            onChangeText={value=> setSearch(value)}
+            style={styles.searchInput}
+          />
+          {
+            search && (
+              <Pressable style={styles.closeIcon}>
+                    <Ionicons name='close' size={24} color={theme.colors.neutral(0.6)} />
+              </Pressable>
+            )
+          }
+      </View>
+    </ScrollView>
+
     </View>
   )
 }
@@ -41,6 +72,32 @@ const styles = StyleSheet.create({
     fontSize: hp(4),
     fontWeight: theme.fontWeights.semibold,
     color: theme.colors.neutral(0.9)
+  },
+  searchBar: {
+    marginHorizontal: wp(4),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: theme.colors.grayBG,
+    backgroundColor: theme.colors.white,
+    padding: 6,
+    paddingLeft: 10,
+    borderRadius: theme.radius.lg,
+  },
+  searchIcon: {
+    padding: 0,
+  },
+  searchInput: {
+    flex: 1,
+    borderRadius: theme.radius.sm,
+    paddingVertical: 10,
+    fontSize: hp(1.8),
+  },
+  closeIcon: {
+    backgroundColor: theme.colors.neutral(0.1),
+    padding: 8,
+    borderRadius: theme.radius.sm,
   }
 })
 
