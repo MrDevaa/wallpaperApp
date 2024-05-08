877700dd184c71f561f580ac956a914f23daeef4
Devanandhan Codandabany <d.codandabany@gmail.com>
2024-05-08 22:54:08 +0200

    added image modal when click | soon to be fixed

--- app/home/image.js
+++ app/home/image.js
@@ -1,16 +1,39 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
 import { BlurView } from 'expo-blur'
 import { wp } from '../../helpers/common'
+import { useLocalSearchParams, useRouter } from 'expo-router'
+import { Image } from 'expo-image'
+import { theme } from '../../constants/theme';
+
 
 const ImageScreen = () => {
+  const router = useRouter();
+  const item = useLocalSearchParams();
+  const [status, setStatus] = useState('');
+  let uri = item?.webformatURL;
+  // console.log('image:', item);
+
+
+  const onLoad = () => {
+      setStatus('');
+  }
+
   return (
     <BlurView
         style={styles.container}
         tint='dark'
         intensity={60}
-
     >
+      <View style={[]}>
+        <Image 
+              transition={100}
+              style={[styles.image]}
+              source={uri}
+              onLoad={onLoad}
+        />
+      </View>
+      <Button title='Back' onPress={()=> router.back()} />
     </BlurView>
   )
 }
@@ -22,8 +45,15 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         paddingHorizontal: wp(4),
+        backgroundColor: 'rgba(0,0,0,0.5)'
+    },
+    image: {
+      borderRadius: theme.radius.lg,
+      borderWidth: 2,
+      backgroundColor: 'rgba(225,225,225,0.1)',
+      borderColor: 'rgba(225,225,225,0.1)',
 
-    }
+      }
 })
 
 export default ImageScreen
